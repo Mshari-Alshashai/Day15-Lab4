23c579a55625480cbd2dfbef6b2a83d8398f3bfd
Mshari-Alshashai <msharialshashai@gmail.com>
2024-11-07 13:08:12 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -31,17 +31,16 @@ public class Main {
 
         System.out.println("Areas: ");
 
-        System.out.println("Rectangle: "+r.calculateArea());
-        System.out.println("Circle: "+c.calculateArea());
-        System.out.println("Triangle: "+t.calculateArea());
+        r.calculateArea();
+        c.calculateArea();
+        t.calculateArea();
 
         System.out.println("===========================================");
 
         System.out.println("Perimeter: ");
-
-        System.out.println("Rectangle: "+r.calculateCircumference());
-        System.out.println("Circle: "+c.calculateCircumference());
-        System.out.println("Triangle: "+t.calculateCircumference());
+        r.calculateCircumference();
+        c.calculateCircumference();
+        t.calculateCircumference();
 
     }
 }
